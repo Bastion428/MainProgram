79fe5f35ec99dd24b7c5e4351c6bf238c06816d1
Sebastian Suarez <cbass428@gmail.com>
2024-07-11 15:14:11 -0400

    Added secure user authentication

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "game_collection.db"
@@ -22,5 +22,13 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id_num):
+        return User.query.get(int(id_num))
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -9,25 +13,55 @@ def sign_up():
         username = request.form.get('username')
         pass1 = request.form.get('password1')
         pass2 = request.form.get('password2')
+        error = False
 
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already is use', category='error')
+            error = True
         if pass1 != pass2:
             flash('Passwords do not match', category='error')
-        elif len(pass1) < 6:
+            error = True
+        if len(pass1) < 6:
             flash('Password must be 6 characters or more', category='error')
-        elif len(username) < 6:
+            error = True
+        if len(username) < 6:
             flash('Username must be 6 characters or more', category='error')
-        else:
+            error = True
+        if not error:
+            new_user = User(username=username,  # type: ignore[call-arg]
+                            password=generate_password_hash(pass1, method='pbkdf2:sha256'))  # type: ignore[call-arg]
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash('Succesfully created account!', category='success')
+            return redirect(url_for('auth.login'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
 
-@auth.route('/', methods=["GET", "POST"])
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    info = request.form
-    return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Successfully logged in', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.collection'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Username does not exist', category='error')
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("auth.login"))
