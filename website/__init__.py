3acf424a082033b59306ee0220419c8b90c51c41
Sebastian Suarez <cbass428@gmail.com>
2024-07-11 11:44:03 -0400

    Remove .idea from repo

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "game_collection.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'FD8hf8d8yfdas*F89dfhsd*FT&DhFDhxy7^#$9cvhd7^DD^Fh3ud87&DF'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, MyGame
+
+    with app.app_context():
+        db.create_all()
 
     return app
---
+++ website/models.py
@@ -0,0 +1,24 @@
+from . import db
+from flask_login import UserMixin
+
+
+class MyGame(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    platform = db.Column(db.String(100), nullable=False)
+    developer = db.Column(db.String(100))
+    publisher = db.Column(db.String(100))
+    image = db.Column(db.String(1000))
+    play_hours = db.Column(db.Integer)
+    own = db.Column(db.Boolean)
+    beat = db.Column(db.Boolean)
+    review = db.Column(db.String(10000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    games = db.relationship('MyGame')
