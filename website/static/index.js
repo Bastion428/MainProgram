9b9164f77fcce90ab468ebc8ea772649f507f935
Sebastian Suarez <cbass428@gmail.com>
2024-07-19 11:10:24 -0400

    Deleted js function that was not used

--- website/static/index.js
+++
@@ -1,8 +0,0 @@
-function getGame(ID) {
-    fetch("/my-game", {
-        method: "POST",
-        body: JSON.stringify({ id: ID }),
-    }).then((_res) => {
-        console.log("Successfully aquired");
-    });
-}
