b50ff6f6b0a582bc3bb6d2f4615faf2dc0d172a1
Sebastian Suarez <cbass428@gmail.com>
2024-07-29 01:03:33 -0400

    Updated to deal with image column in MyGame table. Dealt with errors returned from put request when trying to update game.

--- website/static/index.js
+++ website/static/index.js
@@ -4,48 +4,55 @@ updateGameForm.addEventListener("submit", function (e) {
    
     e.preventDefault();
 
-	let link = '/update';
+    let link = '/update';
 
-	let inputID = document.getElementById("id").value;
+    let inputID = document.getElementById("id").value;
     let inputTitle = document.getElementById("title").value;
     let inputYear = document.getElementById("year").value;
     let inputPlatform = document.getElementById("platform").value;
     let inputDeveloper = document.getElementById("developer").value;
     let inputPublisher = document.getElementById("publisher").value;
+    let inputImage = document.getElementById('image').value;
     let inputHours = document.getElementById("hours").value;
     let inputScore = document.getElementById("score").value;
     let inputOwn = document.getElementById("own-edit").value;
-	let inputBeat = document.getElementById("beat-edit").value;
+    let inputBeat = document.getElementById("beat-edit").value;
     let inputReview = document.getElementById("review").value;
 
-	console.log(inputTitle);
 
     let data = {
-		id: inputID,
+        id: inputID,
         title: inputTitle,
         year: inputYear,
         platform: inputPlatform,
+        image: inputImage,
         developer: inputDeveloper,
         publisher: inputPublisher,
         play_hours: inputHours,
         score: inputScore,
         own: inputOwn,
-		beat: inputBeat,
-		review: inputReview
+    beat: inputBeat,
+    review: inputReview
     };
-    
+      
 
-	$.ajax({
-		url: link,
-		type: 'PUT',
-		data: JSON.stringify(data),
-		contentType: "application/json; charset=utf-8",
-		dataType: 'json',
-		success: function(result) {
-			console.log(inputOwn);
-			window.location.href = '/';
-		}
-	});
+    $.ajax({
+        url: link,
+        type: 'PUT',
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: 'text',
+        success: function(result) {
+            window.location.href = '/';
+        },
+        error: function(request,status,errorThrown) {
+            alert(`Error (${request.status}): ${request.responseText}`);
+            if (request.status !== 400) {
+                window.location.href = '/';
+            }
+
+       }
+    });
 
 })
 
@@ -62,28 +69,28 @@ function deleteGame(ID, location) {
     }
   
     $.ajax({
-      url: link,
-      type: 'DELETE',
-      data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8",
-      dataType: 'json',
-      success: function(result) {
-        deleteRow(ID);
-      }
+        url: link,
+        type: 'DELETE',
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function(result) {
+            deleteRow(ID);
+        }
     });
-  }
+}
 
 
 function deleteRow(ID){
-	let table = document.getElementById("my_games_table");
+    let table = document.getElementById("my_games_table");
 
-	if (table === null) {
-		window.location.href = "/";
-	}
-	for (let i = 0, row; row = table.rows[i]; i++) {
-		if (table.rows[i].getAttribute("data-value") == ID) {
-			table.deleteRow(i);
-			break;
-		}
-	}
+    if (table === null) {
+        window.location.href = "/";
+    }
+    for (let i = 0, row; row = table.rows[i]; i++) {
+        if (table.rows[i].getAttribute("data-value") == ID) {
+            table.deleteRow(i);
+            break;
+        }
+    }
 }
