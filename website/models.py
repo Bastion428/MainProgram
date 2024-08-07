478a04aab02cee51db1d2dbd6949722ba997e726
Sebastian Suarez <cbass428@gmail.com>
2024-08-07 12:46:23 -0400

    Added name, email fields to user table

--- website/models.py
+++ website/models.py
@@ -20,6 +20,8 @@ class MyGame(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     games = db.relationship('MyGame')
